80423a0db610526b68905b3cf47f24ea0fd7bf60
Justin Heath <just.coder@outlook.com>
2021-08-20 10:08:18 +0100

    TS added to example 1

--- src/app/Routings/example1/Model/UserModel.ts
+++ src/app/Routings/example1/Model/UserModel.ts
@@ -1,7 +1,15 @@
 export interface I_User {
-	userId: number;
-	firstName: string;
-	lastName: string;
-	phoneNumber: string;
-	emailAddress: string;
+  // declaring a structure
+  userId: number;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  emailAddress: string;
+}
+
+export interface I_Example1Component {
+  title: string; // variable(property as in a class) assignment
+  UserList: I_User[]; // variable(property as in a class) assignment
+  removeUser(id: number): void; // call signature
+  onSubmit(data: I_User): void; // call signature
 }
--- src/app/Routings/example1/example1.component.ts
+++ src/app/Routings/example1/example1.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
-import Users from '../../../assets/db-data.json';
-import { I_User } from './model/UserModel';
+import Users from '../../../assets/db-data.json'; // import the data from local JSON file.
+import { I_User, I_Example1Component } from './model/UserModel'; // TS - Created an interface for the data.
 
 @Component({
   selector: 'app-example1',
   templateUrl: './example1.component.html',
   styleUrls: ['./example1.component.css'],
 })
-export class Example1Component implements OnInit {
-  public UserList: I_User[] = Users;
+export class Example1Component implements OnInit, I_Example1Component {
+  public title: string = `Example of working within 1 component`;
+  public UserList: I_User[] = Users; // TS - put 'public' which is the default modifier but just wanted to get use to seeing them.
+  // TS - Set the structure with interface and initialised with the actual data.
 
   constructor() {}
 
   ngOnInit(): void {}
 
   removeUser(id: number): void {
+    // TS - Declared the type on the argument and set to void as no return.
     // console.log(`Delete Button Working`)
     this.UserList = this.UserList.filter((user) => user.userId !== id);
   }
 
-  onSubmit(data: any) {
+  onSubmit(data: I_User): void {
+    // TS - Declared the type on the argument and set to void as no return.
     // e.preventDefault()
     data.userId = this.UserList.length + 1;
     // console.log(data)
